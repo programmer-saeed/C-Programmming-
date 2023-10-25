5e83ae9ee46c5a22f1f909be75b6262abc0767fb
CodeFather-Saeed <kazisaeed8056@gmail.com>
2023-10-25 21:15:31 +0600

    test init

---
+++ input output .cpp
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{ /*
+    int a,b;
+
+    printf("Enter first number = ");
+    scanf("%d",&a);
+
+    printf("Enter second number = ");
+    scanf("%d",&b);
+
+    int sum = a + b;
+    int sub = a - b;
+    int mul = a * b;
+    double div = a / b;
+    int mod = a % b;
+
+    printf("Summation is = %d\n",sum);
+    printf("Subtraction is = %d\n",sub);
+    printf("Multiplication is = %d\n",mul);
+    printf("Division is = %0.3lf\n",div);
+    printf("Modulus is = %d\n",mod);
+
+    return 0;
+    */
+
+    int marks = 100;
+    int price = 50;
+    float temparature = 50;
+
+    printf("I have got %d marks and price %d taka and temaprature %f",marks,price,temparature);
+
+    return 0;
+}
